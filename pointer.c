177d3587fdb7679bfde7b7e9fdd8a082a16b6bd1
rashisweet1995 <rashisweet1995@gmail.com>
2016-06-24 19:55:18 +0530

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int *a ,*b,sum;
+printf("\n enter the value for a and b");
+scanf("%d%d",a,b);
+sum=*a + *b;
+printf("\n the sum are %d",sum);
+getch();
+}
